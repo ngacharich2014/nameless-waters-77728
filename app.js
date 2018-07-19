5a9b93bc33e0e918c211d19d641ba8849ff3c711
Nicholas Alexander Gacharich <35305711+ngacharich2014@users.noreply.github.com>
2018-07-19 00:12:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -113,4 +113,22 @@ angular.module("contactsApp", ['ngRoute'])
         $scope.deleteContact = function(contactId) {
             Contacts.deleteContact(contactId);
         }
+
+        var upload = multer({dest: "./uploads"});
+var mongo = require('mongodb');
+var Grid = require("gridfs-stream");
+Grid.mongo = mongo;
+
+    router.post('/:id', upload.array('photos', 200), function(req, res, next){
+    gfs = Grid(db);
+    var ss = req.files;
+    for(var j=0; j<ss.length; j++){
+        var originalName = ss[j].originalname;
+        var filename = ss[j].filename;
+        var writestream = gfs.createWriteStream({
+            filename: originalName
+        });
+        fs.createReadStream("./uploads/" + filename).pipe(writestream);
+    }
     });
+});
